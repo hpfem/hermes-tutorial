d465f62e33a114fd79839c30bc61c96daa59bbb1
Pavel Solin <solin.pavel@gmail.com>
2011-07-30 16:50:22 -0400

    Templating example P02/02.

---
+++ P02-nonlinear/02-newton-analytic/main.cpp
@@ -0,0 +1,99 @@
+#define HERMES_REPORT_ALL
+#define HERMES_REPORT_FILE "application.log"
+#include "definitions.h"
+#include "function/function.h"
+
+using namespace RefinementSelectors;
+
+//  This example solves the same nonlinear problem as the previous
+//  one but now using the Newton's method.
+//
+//  PDE: Stationary heat transfer equation with nonlinear thermal
+//       conductivity, - div[lambda(u) grad u] + src(x, y) = 0.
+//
+//  Nonlinearity: lambda(u) = 1 + pow(u, alpha).
+//
+//  Domain: square (-10, 10)^2.
+//
+//  BC: Nonconstant Dirichlet.
+//
+//  The following parameters can be changed:
+
+const int P_INIT = 2;                             // Initial polynomial degree.
+const double NEWTON_TOL = 1e-6;                   // Stopping criterion for the Newton's method.
+const int NEWTON_MAX_ITER = 100;                  // Maximum allowed number of Newton iterations.
+const int INIT_GLOB_REF_NUM = 3;                  // Number of initial uniform mesh refinements.
+const int INIT_BDY_REF_NUM = 4;                   // Number of initial refinements towards boundary.
+MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESOS, SOLVER_AZTECOO, SOLVER_MUMPS,
+                                                  // SOLVER_PETSC, SOLVER_SUPERLU, SOLVER_UMFPACK.
+
+// Problem parameters.
+double heat_src = 1.0;
+double alpha = 4.0;
+
+int main(int argc, char* argv[])
+{
+  // Load the mesh.
+  Mesh mesh;
+  H2DReader mloader;
+  mloader.load("square.mesh", &mesh);
+
+  // Perform initial mesh refinements.
+  for(int i = 0; i < INIT_GLOB_REF_NUM; i++) mesh.refine_all_elements();
+  mesh.refine_towards_boundary("Bdy", INIT_BDY_REF_NUM);
+
+  // Initialize boundary conditions.
+  CustomEssentialBCNonConst bc_essential("Bdy");
+  EssentialBCs<double> bcs(&bc_essential);
+
+  // Create an H1 space with default shapeset.
+  H1Space<double> space(&mesh, &bcs, P_INIT);
+  int ndof = space.get_num_dofs();
+  info("ndof: %d", ndof);
+
+  // Initialize the weak formulation
+  CustomNonlinearity lambda(alpha);
+  HermesFunction<double> src(-heat_src);
+  DefaultWeakFormPoisson<double> wf(HERMES_ANY, &lambda, &src);
+
+  // Initialize the FE problem.
+  DiscreteProblem<double> dp(&wf, &space);
+
+  // Project the initial condition on the FE space to obtain initial 
+  // coefficient vector for the Newton's method.
+  // NOTE: If you want to start from the zero vector, just define 
+  // coeff_vec to be a vector of ndof zeros (no projection is needed).
+  info("Projecting to obtain initial vector for the Newton's method.");
+  double* coeff_vec = new double[ndof];
+  CustomInitialCondition init_sln(&mesh);
+  OGProjection<double>::project_global(&space, &init_sln, coeff_vec, matrix_solver); 
+
+  // Initialize Newton solver.
+  NewtonSolver<double> newton(&dp, matrix_solver);
+
+  // Perform Newton's iteration.
+  if (!newton.solve(coeff_vec)) 
+    error("Newton's iteration failed.");
+
+  // Translate the resulting coefficient vector into a Solution.
+  Solution<double> sln;
+  Solution<double>::vector_to_solution(newton.get_sln_vector(), &space, &sln);
+
+  // Get info about time spent during assembling in its respective parts.
+  //dp.get_all_profiling_output(std::cout);
+
+  // Clean up.
+  delete [] coeff_vec;
+
+  // Visualise the solution and mesh.
+  ScalarView<double> s_view("Solution", new WinGeom(0, 0, 440, 350));
+  s_view.show_mesh(false);
+  s_view.show(&sln);
+  OrderView<double> o_view("Mesh", new WinGeom(450, 0, 400, 350));
+  o_view.show(&space);
+
+  // Wait for all views to be closed.
+  View::wait();
+  return 0;
+}
+
