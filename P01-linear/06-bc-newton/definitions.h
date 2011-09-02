d7fb65f51662ae3d74012e713873ebcb933b437a
Pavel Solin <solin.pavel@gmail.com>
2011-09-01 21:12:38 -0700

    Templating example 06-bc-newton.

---
+++ P01-linear/06-bc-newton/definitions.h
@@ -0,0 +1,33 @@
+#include "hermes2d.h"
+
+using namespace Hermes;
+using namespace Hermes::Hermes2D;
+using namespace Hermes::Hermes2D::WeakFormsH1;
+using namespace Hermes::Hermes2D::Views;
+
+/* Weak forms */
+
+class CustomWeakFormPoissonNewton : public WeakForm<double>
+{
+public:
+  CustomWeakFormPoissonNewton(std::string mat_al, Hermes1DFunction<double>* lambda_al,
+                              std::string mat_cu, Hermes1DFunction<double>* lambda_cu,
+                              Hermes2DFunction<double>* vol_src_term, std::string bdy_heat_flux,
+                              double alpha, double t_exterior);
+};
+
+/* Custom non-constant Dirichlet condition */
+
+class CustomDirichletCondition : public EssentialBoundaryCondition<double> 
+{
+public:
+  CustomDirichletCondition(Hermes::vector<std::string> markers, double A, double B, double C);
+
+  virtual EssentialBCValueType get_value_type() const;
+
+  virtual double value(double x, double y, double n_x, double n_y, double t_x, double t_y) const;
+
+  protected:
+    double A, B, C;
+};
+
