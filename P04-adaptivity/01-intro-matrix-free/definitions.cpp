b4896931bfe0e63c22f3565fe479ac63d81229aa
Pavel Solin <solin.pavel@gmail.com>
2011-09-11 14:15:28 -0700

    Improved NOX initialization in example 01-intro-matrix-free.

--- P04-adaptivity/01-intro-matrix-free/definitions.cpp
+++ P04-adaptivity/01-intro-matrix-free/definitions.cpp
@@ -1,8 +1,10 @@
 #include "definitions.h"
 
 CustomWeakFormPoisson::CustomWeakFormPoisson(const std::string& mat_motor, double eps_motor, 
-                                             const std::string& mat_air, double eps_air) : WeakForm<double>(1)
+                                             const std::string& mat_air, double eps_air, bool is_matfree) : WeakForm<double>(1)
 {
+  this->is_matfree = is_matfree;
+
   // Jacobian.
   add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<double>(0, 0, mat_motor, new Hermes1DFunction<double>(eps_motor)));
   add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<double>(0, 0, mat_air, new Hermes1DFunction<double>(eps_air)));
