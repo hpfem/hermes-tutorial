f0ed2da513001de7a669e6e7a7fdb68902429f96
Pavel Solin <solin.pavel@gmail.com>
2011-09-11 21:05:29 -0700

    Templating P03/02.

---
+++ P03-transient/02-runge-kutta/definitions.cpp
@@ -0,0 +1,72 @@
+#include "definitions.h"
+
+double CustomInitialCondition::value(double x, double y) const 
+{
+  return const_value;
+}
+
+void CustomInitialCondition::derivatives(double x, double y, double& dx, double& dy) const 
+{   
+  dx = 0;
+  dy = 0;
+}
+
+Ord CustomInitialCondition::ord(Ord x, Ord y) const 
+{
+  return Ord(0);
+}
+
+CustomWeakFormHeatRK::CustomWeakFormHeatRK(std::string bdy_air, double alpha, double lambda, double heatcap, double rho,
+                                           double* current_time_ptr, double temp_init, double t_final) : WeakForm(1)
+{
+  // Jacobian volumetric part.
+  add_matrix_form(new DefaultJacobianDiffusion<double>(0, 0, HERMES_ANY, new Hermes1DFunction<double>(-lambda / (heatcap * rho))));
+
+  // Jacobian surface part.
+  add_matrix_form_surf(new DefaultMatrixFormSurf<double>(0, 0, bdy_air, new Hermes2DFunction<double>(-alpha / (heatcap * rho))));
+
+  // Residual - volumetric.
+  add_vector_form(new DefaultResidualDiffusion<double>(0, HERMES_ANY, new Hermes1DFunction<double>(-lambda / (heatcap * rho))));
+
+  // Residual - surface.
+  add_vector_form_surf(new CustomFormResidualSurf(0, bdy_air, alpha, rho, heatcap,
+                       current_time_ptr, temp_init, t_final));
+}
+
+double CustomWeakFormHeatRK::CustomFormResidualSurf::value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e,
+                                                           ExtData<double> *ext) const 
+{
+  double T_ext = temp_ext(get_current_stage_time());
+  double result = 0;
+
+  for (int i = 0; i < n; i++) 
+  {
+    result += wt[i] * (T_ext - u_ext[0]->val[i]) * v->val[i];
+  }
+
+  return alpha / (rho * heatcap) * result;
+}
+
+Ord CustomWeakFormHeatRK::CustomFormResidualSurf::ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const 
+{
+  Ord T_ext;
+  Ord result;
+
+  for (int i = 0; i < n; i++) 
+  {
+    result += wt[i] * (T_ext - u_ext[0]->val[i]) * v->val[i];
+  }
+
+  return alpha / (rho * heatcap) * result;
+}
+
+VectorFormSurf<double>* CustomWeakFormHeatRK::CustomFormResidualSurf::clone() 
+{
+  return new CustomFormResidualSurf(*this);
+}
+
+template<typename Real>
+Real CustomWeakFormHeatRK::CustomFormResidualSurf::temp_ext(Real t) const 
+{
+  return temp_init + 10. * Hermes::sin(2*M_PI*t/t_final);
+}
---
+++ P03-transient/02-runge-kutta/definitions.h
@@ -0,0 +1,63 @@
+#include "hermes2d.h"
+
+using namespace Hermes;
+using namespace Hermes::Hermes2D;
+using namespace Hermes::Hermes2D::WeakFormsH1;
+using namespace Hermes::Hermes2D::Views;
+
+/* Initial condition */
+
+class CustomInitialCondition : public ExactSolutionScalar<double>
+{
+public:
+  CustomInitialCondition(Mesh* mesh, double const_value) : ExactSolutionScalar<double>(mesh), 
+    const_value(const_value)
+  {
+  };
+
+  virtual double value(double x, double y) const;
+
+  virtual void derivatives(double x, double y, double& dx, double& dy) const;
+
+  virtual Ord ord(Ord x, Ord y) const;
+
+  double const_value;
+};
+
+/* Weak forms */
+
+class CustomWeakFormHeatRK : public WeakForm<double>
+{
+public:
+  CustomWeakFormHeatRK(std::string bdy_air, double alpha, double lambda, double heatcap, double rho,
+                       double* current_time_ptr, double temp_init, double t_final);
+
+private:
+  // This form is custom since it contains time-dependent exterior temperature.
+  class CustomFormResidualSurf : public VectorFormSurf<double>
+  {
+  private:
+      double h;
+  public:
+    CustomFormResidualSurf(int i, std::string area, double alpha, double rho,
+                           double heatcap, double* current_time_ptr, double temp_init, double t_final)
+          : VectorFormSurf(i, area), alpha(alpha), rho(rho),
+                                     heatcap(heatcap), current_time_ptr(current_time_ptr),
+                                     temp_init(temp_init), t_final(t_final) {};
+
+    virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e,
+                         ExtData<double> *ext) const;
+
+    virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
+
+    virtual VectorFormSurf<double>* clone();
+
+    // Time-dependent exterior temperature.
+    template<typename Real>
+    Real temp_ext(Real t) const;
+
+    // Members.
+    double alpha, rho, heatcap, *current_time_ptr, temp_init, t_final;
+  };
+};
+
