fe10c7a042724d788508742cde2db3b654ac4fa6
Lukas Korous <lukas.korous@gmail.com>
2011-11-13 18:32:49 +0100

    Fix of P01-03.

--- P01-linear/03-poisson/main.cpp
+++ P01-linear/03-poisson/main.cpp
@@ -109,9 +109,6 @@ int main(int argc, char* argv[])
   Solution<double> sln;
   Solution<double>::vector_to_solution(newton.get_sln_vector(), &space, &sln);
 
-  // Get info about time spent during assembling in its respective parts.
-  dp.get_all_profiling_output(std::cout);
-
   // VTK output.
   if (VTK_VISUALIZATION) 
   {
