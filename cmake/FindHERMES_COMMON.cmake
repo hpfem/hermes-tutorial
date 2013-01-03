76d0de3dbffcd61dd8165421e907b1572f540932
Lukas <lukas.korous@gmail.com>
2013-01-03 19:27:18 +0100

    A tiny fix in cmake.

--- cmake/FindHERMES_COMMON.cmake
+++ cmake/FindHERMES_COMMON.cmake
@@ -1,4 +1,4 @@
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_LIBRARY(HERMES_COMMON_LIBRARY NAMES hermes_common hermes_common-debug PATHS ${HERMES_DIRECTORY} /usr/lib /usr/local/lib)
-FIND_PATH(HERMES_COMMON_INCLUDE hermes_common.h ${HERMES_COMMON_INCLUDE_PATH} /usr/include /usr/local/include)
+FIND_PATH(HERMES_COMMON_INCLUDE hermes_common.h ${HERMES_COMMON_INCLUDE_PATH} /usr/include/hermes_common /usr/local/include/hermes_common)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(HERMES_COMMON DEFAULT_MSG HERMES_COMMON_LIBRARY HERMES_COMMON_INCLUDE)
