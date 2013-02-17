63574381af09a6d4ec21bcffe6c1d1de09e9bb03
Lukas Korous <lukas.korous@gmail.com>
2013-02-17 22:20:35 +0100

    Enable openmp for examples.

--- cmake/CommonTargetProperties.cmake
+++ cmake/CommonTargetProperties.cmake
@@ -7,6 +7,14 @@ macro(SET_COMMON_TARGET_PROPERTIES TRGT HERMES_VERSION)
 	set(HERMES_VERSION ${HERMES_VERSION})
 		
   find_package(HERMES REQUIRED)
+  
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(CMAKE_CXX_FLAGS "-fopenmp ${CMAKE_CXX_FLAGS}")
+  endif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+  if(MSVC)
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /openmp")
+  endif(MSVC)
 
 	target_link_libraries(${TRGT} ${HERMES_COMMON_LIBRARY})
 	target_link_libraries(${TRGT} ${HERMES_LIBRARY})
