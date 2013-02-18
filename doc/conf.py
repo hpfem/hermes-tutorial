34548aee0adc1e63b8f8e0648a086493c9727d71
Lukas Korous <lukas.korous@gmail.com>
2013-02-18 16:14:53 +0100

    Documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -285,7 +285,7 @@ html_theme_path = ['']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Hermes Documentation'
+html_title = 'Hermes Tutorial Guide'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = 'Content'
